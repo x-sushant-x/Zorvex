81963215b1005bf600b84de979990e48d6996d5d
Sushant <sushant.dhiman9812@gmail.com>
2023-12-25 23:39:19 +0530

    Optimized code for table generator

--- CLI/cmd/services.go
+++ CLI/cmd/services.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/fatih/color"
 	"github.com/rodaine/table"
-	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/sushant102004/Zorvex/CLI/services"
 	"github.com/sushant102004/Zorvex/CLI/utils"
@@ -16,16 +15,25 @@ var (
 	columnFmt = color.New(color.FgYellow).SprintfFunc()
 )
 
-func GetAllServices(cmd *cobra.Command, args []string) {
+func tableGenerator(tableHeadings ...string) table.Table {
 	fmt.Println()
+	interfaceHeadings := make([]interface{}, len(tableHeadings))
+	for i, v := range tableHeadings {
+		interfaceHeadings[i] = v
+	}
 
-	tbl := table.New("ID", "Name", "HTTP Method", "IP", "Port", "Balancing Method", "Last Sync", "Endpoint", "Status")
+	tbl := table.New(interfaceHeadings...)
 	tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+	return tbl
+}
 
+func GetAllServices(cmd *cobra.Command, args []string) {
 	services, err := services.GetAllServices()
 	if err != nil {
 		utils.Error(err.Error())
 	}
+
+	tbl := tableGenerator("ID", "Name", "HTTP Method", "IP", "Port", "Balancing Method", "Last Sync", "Endpoint", "Status")
 
 	for _, service := range services {
 		tbl.AddRow(service.ID, service.Name, service.HTTPMethod, service.IPAddress, service.Port, service.LoadBalancingMethod, service.LastSyncTime, service.Endpoint, service.Status)
@@ -37,18 +45,12 @@ func GetAllServices(cmd *cobra.Command, args []string) {
 }
 
 func GetAllDownServices(cmd *cobra.Command, args []string) {
-	tbl := table.New("ID", "Name", "HTTP Method", "IP", "Port", "Balancing Method", "Last Sync", "Endpoint", "Status")
-	tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
-
 	services, err := services.GetAllDownServices()
 	if err != nil {
 		utils.Error(err.Error())
 	}
 
-	if len(services) == 0 {
-		log.Info().Msg("All services are up! 😊")
-		return
-	}
+	tbl := tableGenerator("ID", "Name", "HTTP Method", "IP", "Port", "Balancing Method", "Last Sync", "Endpoint", "Status")
 
 	for _, service := range services {
 		tbl.AddRow(service.ID, service.Name, service.HTTPMethod, service.IPAddress, service.Port, service.LoadBalancingMethod, service.LastSyncTime, service.Endpoint, service.Status)
